f441c97be72e95e764a64cedb988c917fe0323d3
Muhammad Nur Arif <73942198+Arifns18@users.noreply.github.com>
2025-09-04 00:25:54 +0800

    Connect Database local (#2)

--- limited_time/src/pages/Home.vue
+++ limited_time/src/pages/Home.vue
@@ -1,7 +1,31 @@
 <script></script>
+
 <template>
   <div>
-    <h1>Home Page</h1>
-    <p>Welcome to the homepage!</p>
+    <h1>Users</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+    </ul>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Home",
+  data() {
+    return { users: [] }
+  },
+  async created() {
+    try {
+      const res = await axios.get("http://localhost:5000/api/users");
+      this.users = res.data;
+      console.log(this.users);
+    } catch (err) {
+      console.error("Error fetching users:", err);
+    }
+  }
+}
+</script>
+
